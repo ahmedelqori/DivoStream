f01675cc8d224026150257a0771e126cf388b96a
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2024-11-24 21:41:45 +0100

    Setup server

---
+++ server/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({ envFilePath: '.env' ,isGlobal:true}), AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
